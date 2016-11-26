7216d230065197cad8878868b332134f2c39e246
Philip Arvidsson <philip@philiparvidsson.com>
2016-11-26 10:30:44 +0100

    fswatcher doc

--- make.py
+++ make.py
@@ -1,17 +1,10 @@
-#---------------------------------------
-# IMPORTS
-#---------------------------------------
-
 import os, sys
 sys.path.insert(0, os.path.join('build', 'pymake2'))
 
 from pymake2 import *
 
 from pymake2.template.latex import pdflatex
-
-#---------------------------------------
-# SCRIPT
-#---------------------------------------
+from pymake2.template.util  import fswatcher
 
 pymake2({
     'name'    : 'thesis',
