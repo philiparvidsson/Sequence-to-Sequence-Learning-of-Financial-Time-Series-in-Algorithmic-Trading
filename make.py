0fa207876387d143662d8c9546c49b9b65637eb9
Philip Arvidsson <philip@philiparvidsson.com>
2016-12-02 15:20:59 +0100

    better bibtex in make script

--- make.py
+++ make.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os, sys
 sys.path.insert(0, os.path.join('build', 'pymake2'))
 
@@ -12,8 +13,27 @@ def all(conf):
 
 @after_target('compile')
 def bibtex(conf):
-    name = os.path.join(conf.bindir, conf.name)
-    run_program('bibtex', [ '--include-directory', conf.srcdir, name ])
+    bindir = os.path.abspath(conf.bindir)
+
+    cwd = os.getcwd()
+    os.chdir(conf.srcdir)
+
+    bindir = os.path.relpath(bindir, os.getcwd())
+    name = os.path.join(bindir, conf.name)
+
+    run_program('bibtex', [ name ])
+
+    os.chdir(cwd)
+
+    # Set the modified time and rerun pdflatex to insert citations properly.
+
+    srcfile = os.path.join(conf.srcdir, conf.srcfile)
+
+    os.utime(srcfile, None)
+    pdflatex.compile(conf)
+
+    os.utime(srcfile, None)
+    pdflatex.compile(conf)
 
 pymake2({
     'name'    : 'thesis',
